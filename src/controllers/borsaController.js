08ef9278ef952492790e8700a346603fc0c27cd4
emrebuzkiran <emre-04gs@hotmail.com>
2024-02-21 02:58:42 +0300

    Refactor borsaController.js to improve readability and maintainability

--- src/controllers/borsaController.js
+++ src/controllers/borsaController.js
@@ -43,78 +43,40 @@ const borsa30Controller = function (req, res, next) {
 };
 
 const dataParse = (error, response, body, res) => {
-  var allStocks = [];
-  if (error || res.statusCode !== 200) return errorResponse(res, 'Server Error', 500);
+  if (error || res.statusCode !== 200) {
+    return errorResponse(res, "Server Error", 500);
+  }
 
-      try {
-          let $ = cheerio.load(body);
+  try {
+    const $ = cheerio.load(body);
+    const allStocks = [];
 
-          $('#acik_koyu_yeri tbody .zebra').each(function (i, elem) {
-              if ($(this).text() !== null && $(this).text() !== '')
-                  allStocks[i] = $(this).text();
-          });
-
-          $('#acik_koyu_yeri2 tbody .zebra').each(function (i, elem) {
-              if ($(this).text() !== null && $(this).text() !== '')
-                  allStocks.push($(this).text());
-          });
-
-          $('#acik_koyu_yeri3 tbody .zebra').each(function (i, elem) {
-              if (
-                  $(this).text() !== null &&
-                  $(this).text() !== '' &&
-                  $(this).text() !== ' '
-              )
-                  allStocks.push($(this).text());
-          });
-
-          var borsaName = [];
-          var borsaZaman = [];
-          var borsa = [];
-
-          for (var i = 0; i < allStocks.length; i++) {
-              borsaName.push(allStocks[i].match(/([a-zA-Z])\w+/g));
-              borsa.push(allStocks[i].match(/[-]?([0-9]+,)\w+/g));
-              borsaZaman.push(allStocks[i].match(/(\d{2}):(\d{2}):(\d{2})/g));
-          }
-
-          var borsaFiyat = [];
-          var borsaDegisim = [];
-
-          var borsa = borsa.filter(function (el) {
-              return el != null;
-          });
-
-          for (var i = 0; i < borsa.length; i++) {
-              borsaFiyat.push(borsa[i][0]);
-              borsaDegisim.push(borsa[i][1]);
-              borsaZaman.push(borsa[i][5]);
-          }
-
-          borsaName.splice(0, 1);
-          borsaName.splice(34, 1);
-          borsaName.splice(68, 1);
-
-          var borsaZaman = borsaZaman.filter(function (el) {
-              return el != null;
-          });
-
-          var result = [];
-          for (var i = 0; i < borsaName.length; i++) {
-              borsaObject = new BorsaFiyatlari(
-                  Array.isArray(borsaName[i]) ? borsaName[i].toString() : borsaName[i],
-                  borsaFiyat[i],
-                  borsaDegisim[i],
-                  Array.isArray(borsaZaman[i]) ? borsaZaman[i].toString() : borsaZaman[i],
-              );
-              result.push(borsaObject);
-          }
-          successResponse(res, result);
-      }catch (error){
-          console.log(error)
-          return errorResponse(res, 'Parse Error', 500);
+    $(
+      "#acik_koyu_yeri tbody .zebra, #acik_koyu_yeri2 tbody .zebra, #acik_koyu_yeri3 tbody .zebra"
+    ).each(function (i, elem) {
+      const stockData = $(this).text().trim();
+      if (stockData !== "") {
+        allStocks.push(stockData);
       }
-}
+    });
+
+    const stocks = [];
+    allStocks.forEach((stock) => {
+      const [name, price, change, time] = stock.split(/\s+/);
+      stocks.push({
+        name: name,
+        price: price,
+        change: change,
+        time: time,
+      });
+    });
+
+    successResponse(res, stocks);
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, "Parse Error", 500);
+  }
+};
 
 
 module.exports = {
